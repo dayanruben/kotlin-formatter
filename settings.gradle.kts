cf2930294e287154331372b89c2a6a8155bd894c
Kartikaya Gupta <kartikaya@squareup.com>
2025-01-23 16:30:53 -0500

    chore: let the convention plugin apply develocity
    We're applying it two different ways with potentially two
    different versions. Better to let the convention plugin do
    it as it configures it as well.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,8 +10,6 @@ pluginManagement {
 }
 
 plugins {
-  // Keep this version in sync with version catalog
-  id("com.gradle.develocity") version "3.19.1"
   id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
   id("block.settings")
 }
